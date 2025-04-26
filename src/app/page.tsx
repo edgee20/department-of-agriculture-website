b2f59ef6f18e670edde5862a645e44c84148882e
edgee20 <ejfusingan0@gmail.com>
2025-04-26 17:30:26 +0800

    feat: created edp button variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -17,10 +18,11 @@ export default function Home() {
         <div className="absolute inset-0 bg-black/60"></div>
 
         {/* heading title */}
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-[40px] md:text-[80px] font-bold italic text-center max-w-[1230px] px-4">
+        <div className="absolute inset-0 flex flex-col items-center justify-center px-2">
+          <h1 className="text-white text-[40px] md:text-[55px] lg:text-[80px] font-bold italic text-center max-w-[1230px] px-4 pb-4">
             Masaganang Agrikultura, Maunlad na Ekonomiya!
           </h1>
+          <Button>SCHEDULE A MEETING</Button>
         </div>
       </div>
     </main>
