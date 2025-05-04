2aff4df7cdcd40f91196a368236092b349214fcf
dambaculio <dambaculio@addu.edu.ph>
2025-05-05 01:13:31 +0800

    finished the shedule page, fix services, contact us draft

--- src/components/pages/SchedulePage.tsx
+++ src/components/pages/SchedulePage.tsx
@@ -5,6 +5,7 @@ import { Calendar } from "@/components/ui/calendar";
 import SunDiv from "@/components/ui/SunDiv";
 import { BG_CONFIG } from "@/lib/config";
 import React from "react";
+import Accordion from "../ui/Accordion";
 
 export default function SchedulePage() {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
@@ -83,6 +84,45 @@ export default function SchedulePage() {
         <div className="mt-10 text-center">
           <Button className="mt-5 mb-10 h-10 w-70">CONFIRM</Button>
         </div>
+      </div>
+
+      {/*DRAFT FOR CONTACT US PAGE*/}
+      <div className="relative bg-edpgreen overflow-hidden pt-8 pb-8 lg:p-8 flex flex-col items-center mb-7">
+        <div className={`${BG_CONFIG}`} />
+
+        {/* fill-up container for Contact us */}
+        <div className="flex justify-between w-full mt-20 mb-20 text-white text-m [&>h3]:pb-3">
+          <div className="mt-20">
+            {/* Introduction message */}
+            <div className="justify-center mr-30 ml-20 text-xl">
+              <h3>
+                If you have any inquiries or require support, please complete
+                the form. We will respond to you as soon as possible.
+              </h3>
+            </div>
+
+            {/* accordion here */}
+          </div>
+
+          <div className="justify-center mx-20 [&>Input]:w-150 [&>Input]:rounded-xl [&>Input]:bg-white [&>Input]:border-0 [&>h3]:font-bold [&>h3]:mb-3 [&>h3]:text-xl">
+            <h3>Full Name *</h3>
+            <Input type="text" placeholder="Enter name" className="h-13" />
+
+            <h3 className="mt-7">Email *</h3>
+            <Input type="email" placeholder="Enter email" className="h-13" />
+
+            <h3 className="mt-7">Phone Number *</h3>
+            <Input
+              type="number"
+              placeholder="Enter phone number"
+              className="h-13"
+            />
+
+            <h3 className="mt-7">Message *</h3>
+            <Input type="text" className="h-40" />
+          </div>
+        </div>
+        <Button className="mt-7 mb-10 w-70 h-11 text-xl">SUBMIT</Button>
       </div>
     </div>
   );
